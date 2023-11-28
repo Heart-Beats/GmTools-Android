1dcb45866fd3e762b2b1675ad3831cef9e590119
张磊 <zhangl171@chinatelecom.cn>
2023-11-28 19:16:23 +0800

    相关配置优化

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
 	namespace = "com.hl.gmtools"
-	compileSdk = 33
+	compileSdk = 34
 
 	defaultConfig {
 		applicationId = "com.hl.gmtools"
 		minSdk = 24
-		targetSdk = 33
+		targetSdk = 34
 		versionCode = 1
 		versionName = "1.0"
 
